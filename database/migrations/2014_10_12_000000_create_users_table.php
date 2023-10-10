fa610730e4d1c86cb0f2a10c7887af8373416b27
Ayatooo <l.reynard@isi-dsi.fr>
2023-10-10 20:25:42 +0200

    :rocket: User implemented

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Gender;
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignIdFor(Gender::class);
+            $table->foreignIdFor(Role::class);
             $table->rememberToken();
             $table->timestamps();
         });
