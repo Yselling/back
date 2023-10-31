36e8b241dc3c09d375755aa8416cbb74703ca6a2
romainsilvy <romain.silvy06@gmail.com>
2023-10-31 11:14:49 +0100

    add product pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProductRequest;
-use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\ProductRequest;
 use Illuminate\Support\Facades\Cache;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         $cacheKey = 'products-index';
 
-        $data = Cache::remember($cacheKey, now()->addHour(), function () {
-            return ProductResource::collection(Product::all());
-        });
+        $page = $request->input('page', 5);
+        $perPage = $request->input('per_page', 10);
+
+        // $data = Cache::remember($cacheKey, now()->addHour(), function () use ($perPage, $page) {
+        //     // Use the paginate method to retrieve products with pagination
+        //     return Product::paginate($perPage, ['*'], 'page', $page);
+        // });
+
+        $data = Product::with('category')->paginate($perPage, ['*'], 'page', $page);
 
         return response()->json([
-            'data' => $data,
-            'count' => count($data),
+            'data' => $data->items(), // Get the items for the current page
+            'count' => $data->total(), // Total count of products
+            'per_page' => $data->perPage(), // Number of items per page
+            'current_page' => $data->currentPage(), // Current page number
             'message' => 'Success',
             'status' => 200,
         ]);
